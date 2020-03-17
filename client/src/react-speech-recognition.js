96378d44497c991a5b21a83e7d3e8635b6da88f1
Jesse McKinney <jessman51386@gmail.com>
2020-03-17 10:05:10 -0400

    got the start and stop button working and continuous and autostart functioning

--- client/src/react-speech-recognition.js
+++ client/src/react-speech-recognition.js
@@ -1,35 +1,53 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import SpeechRecognition from "react-speech-recognition";
+
 
 const propTypes = {
   // Props injected by SpeechRecognition
   transcript: PropTypes.string,
   resetTranscript: PropTypes.func,
+  startListening: PropTypes.func,
+  stopListening: PropTypes.func,
   browserSupportsSpeechRecognition: PropTypes.bool,
-  continuous: PropTypes.bool
+  // continuous: PropTypes.bool,
+  autoStart: PropTypes.bool
 };
 
 const Dictaphone = ({
   transcript,
   resetTranscript,
+  startListening,
+  stopListening,
+  // continuous,
+  autoStart,
   browserSupportsSpeechRecognition
 }) => {
   if (!browserSupportsSpeechRecognition) {
     return null;
   }
-  console.log()
 
   return (
     <div>
-      {/*<button onClick={}>Start</button>
-      <button onclick={}>Stop</button>*/}
-      <button onClick={resetTranscript}>Reset</button>
+      <br />
+      <button onClick={startListening}>Start Recording</button>
+      <br />
+      <br />
+      <button onClick={stopListening}>Stop Recording</button>
+      <br />
+      <br />
+      <button onClick={resetTranscript}>Reset Records</button>
+      <br />
+      {console.log(propTypes) /**/}
       <span>{transcript}</span>
     </div>
   );
 };
 
+// Dictaphone.continuous = false;
+// This is set to true by default for continuous recording after start button
+// Set to false and it will only record the one sentence it hears
+Dictaphone.autoStart = false;
 Dictaphone.propTypes = propTypes;
 
 export default SpeechRecognition(Dictaphone);
